47badbf2fbe515a7d1aa02325b6b3a1fa7c5f77a
Antonio Nunes <antonionunes@me.com>
2020-03-16 21:52:13 +0100

    Initial Commit

---
+++ Fractions/Fractions.h
@@ -0,0 +1,19 @@
+//
+//  Fractions.h
+//  Fractions
+//
+//  Created by Antonio Nunes on 16/03/2020.
+//  Copyright © 2020 SintraWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fractions.
+FOUNDATION_EXPORT double FractionsVersionNumber;
+
+//! Project version string for Fractions.
+FOUNDATION_EXPORT const unsigned char FractionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fractions/PublicHeader.h>
+
+
